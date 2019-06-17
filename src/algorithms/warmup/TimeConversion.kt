f0d9897f787cbab7dbbb8d308ea9fbe3d13f7470
Benjamin Earley <benjaminearley92@gmail.com>
2019-06-17 16:38:54 -0400

    TimeConversion Cleanup

--- src/algorithms/warmup/TimeConversion.kt
+++ src/algorithms/warmup/TimeConversion.kt
@@ -6,11 +6,9 @@ import java.util.*
  * Complete the timeConversion function below.
  */
 fun timeConversion(s: String): String {
-    val time = s.substring(0..s.length - 3)
+    val (hour, minute, second) = s.substring(0..s.length - 3).split(':')
 
     val meridianShift = s.slice(s.length - 2 until s.length).let { if (it == "PM") 12 else 0 }
-
-    val (hour, minute, second) = time.split(':')
 
     val militaryHour = (if (hour == "12") meridianShift else hour.toInt() + meridianShift).toString().padStart(2, '0')
 
