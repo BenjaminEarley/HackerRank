ec1266df31555efcbf31528fd9750e3335191de4
Benjamin Earley <benjaminearley92@gmail.com>
2019-06-18 11:34:54 -0400

    Grading Students

---
+++ src/algorithms/warmup/GradingStudents.kt
@@ -0,0 +1,32 @@
+package algorithms.warmup
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> =
+    grades.map {
+        if (it < 38) it
+        else {
+            val difference = 5 - (it % 5)
+            if (difference < 3) it + difference
+            else it
+        }
+    }.toTypedArray()
+
+fun main() {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array(gradesCount) { 0 }
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
+}
